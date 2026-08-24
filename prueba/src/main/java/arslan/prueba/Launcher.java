ed163800a278c0975664876555a2d5151a792409
mrmvlthvs <marleneinesmorenovelazquez@gmail.com>
2026-08-23 23:38:22 -0600

    configure java fx

---
+++ prueba/src/main/java/arslan/prueba/Launcher.java
@@ -0,0 +1,9 @@
+package arslan.prueba;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
