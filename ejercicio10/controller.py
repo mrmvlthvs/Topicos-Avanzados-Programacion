671b2bd0a1573b822636b443107ebcc253443b02
mrmvlthvs <marleneinesmorenovelazquez@gmail.com>
2026-09-01 07:55:23 -0600

    Actualizacion a ejercicio 10

--- ejercicio10/controller.py
+++ ejercicio10/controller.py
@@ -1,3 +1,15 @@
+"""
+Tecnológico Nacional de México
+Instituto Tecnológico de León, Campus 1
+Carrera: Ingeniería en Sistemas Computacionales
+Materia: Topicos Avanzados de Programacion
+Horario:
+    Martes y Jueves de 7:00 a 8:45
+    Viernes de 7:50 a 8:40
+Alumno: Marlene Inés Moreno Velázquez
+Ejercicio #10
+Fecha: 29/08/2026
+"""
 from model import Person
 import view
 
