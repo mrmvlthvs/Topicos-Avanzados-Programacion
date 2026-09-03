4db5f3a6258c4190b28464f43ed6e16c417fd3c5
mrmvlthvs <marleneinesmorenovelazquez@gmail.com>
2026-09-03 07:14:40 -0600

    Ejercicio #11

--- ejercicio09/src/view/View.java
+++ ejercicio09/src/view/View.java
@@ -1,8 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Tecnológico Nacional de México
+Instituto Tecnológico de León, Campus 1
+Carrera: Ingeniería en Sistemas Computacionales
+Materia: Topicos Avanzados de Programacion
+Horario:
+    Martes y Jueves de 7:00 a 8:45
+    Viernes de 7:50 a 8:40
+Alumno: Marlene Inés Moreno Velázquez
+Ejercicio #9
+Fecha: 29/08/2026
+*/
 package view;
 
 import javax.swing.*;
---
+++ ejercicio11/framewithpanel/src/FrameWithPanel.java
@@ -0,0 +1,40 @@
+/*
+Tecnológico Nacional de México
+Instituto Tecnológico de León, Campus 1
+Carrera: Ingeniería en Sistemas Computacionales
+Materia: Topicos Avanzados de Programacion
+Horario:
+    Martes y Jueves de 7:00 a 8:45
+    Viernes de 7:50 a 8:40
+Alumno: Marlene Inés Moreno Velázquez
+Ejercicio #11
+Fecha: 29/08/2026
+*/
+import java.awt.*;
+
+public class FrameWithPanel {
+  private Frame f;
+
+  public FrameWithPanel(String title) {
+    f = new Frame(title);
+  }
+  
+  public void launchFrame() {
+    f.setSize(200,200);
+    f.setBackground(Color.blue);
+    f.setLayout(null);  // Override default layout mgr
+
+    Panel pan = new Panel();
+    pan.setSize(100,100);
+    pan.setBackground(Color.yellow);
+
+    f.add(pan);
+    f.setVisible(true);
+  }
+
+  public static void main(String args[]) {
+    FrameWithPanel guiWindow =
+        new FrameWithPanel("Frame with Panel");
+    guiWindow.launchFrame();
+  }
+}
